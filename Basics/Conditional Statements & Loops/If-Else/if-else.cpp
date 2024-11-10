ffbf1c2a59d7376a7dfe96b409c6949b6ce958e3
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-10 22:21:08 +0530

    Add files via upload

---
+++ Basics/Conditional Statements & Loops/If-Else/if-else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = -45;
+
+    if(num > 0){
+        cout<<"Number is positive \n";
+    } else{
+        cout<<"Number is negative \n";
+    }
+
+    return 0;
+}
