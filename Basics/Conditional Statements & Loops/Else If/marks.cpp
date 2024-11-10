bd4ed83da42af04537fe82f21cd26b256d6d4733
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-10 22:43:27 +0530

    Add files via upload

---
+++ Basics/Conditional Statements & Loops/Else If/marks.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cout<<"Enter marks : ";
+    cin>>marks;
+
+    if(marks >= 90){
+        cout<<"Grade A \n";
+    } else if (marks >=80 && marks < 90)
+    {
+        cout<<"Grade B \n";
+    } else {
+        cout<<"Grade C \n";
+    }
+    
+    return 0;
+}
