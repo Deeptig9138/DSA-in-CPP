e49cdce087734ca9e5e7718296a66574572487c9
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-10 22:53:07 +0530

    Add files via upload

---
+++ Basics/Conditional Statements & Loops/Else If/char1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout<<"Enter a character ";
+    cin>>ch;
+
+    if(ch >= 97 &&  ch <= 122){ //ASCII value for lower case is 97 - 122
+        cout<<"Lowercase \n";
+    } else { //ASCII value for upper case is 65 - 90 
+        cout<<"Uppercase \n";
+    }
+    
+    return 0;
+}
