78fb03c77e294e94d47485c3e5b3ca6a6c14023d
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-11 16:09:24 +0530

    Add files via upload

---
+++ Basics/Conditional Statements & Loops/Do-While loop/numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+    int n = 10;
+
+    do
+    {
+        cout<<i <<" ";
+        i++;
+    } while (i <= n);
+    
+    return 0;
+}
