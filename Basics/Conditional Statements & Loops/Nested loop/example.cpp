642af59a657d1ff80a5a11c2d69f3a39682c09f7
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-11 16:37:14 +0530

    example.cpp

--- Basics/Conditional Statements & Loops/Nested loop/example.cpp
+++ Basics/Conditional Statements & Loops/Nested loop/example.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n = 10;
     for(int i = 0; i <= 5; i++){
-        for(int i = 1; i<=n; i++){
+        for(int j = 1; j <= n; j++){
             cout<<"*";
         }
         cout<<endl;
