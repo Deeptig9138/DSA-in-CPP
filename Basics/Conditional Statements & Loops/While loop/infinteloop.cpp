5a553c69b63e29dba58493a8004f16c04758b2d9
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-11 13:19:03 +0530

    Add files via upload

---
+++ Basics/Conditional Statements & Loops/While loop/infinteloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count = 1;
+
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    while (count <= n)
+    {
+        cout<<count <<"\t";
+    }  
+    return 0; 
+}
