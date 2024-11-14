d5e544cd7df7d56de76203eb6c30fcf7081f86f3
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-14 21:08:33 +0530

    Add files via upload

---
+++ Basics/Patterns/Square Pattern/solid_square1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of lines: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int num = 1;
+        for(int j = 0; j < n; j++){
+            cout<<num;
+            num = num + 1;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
