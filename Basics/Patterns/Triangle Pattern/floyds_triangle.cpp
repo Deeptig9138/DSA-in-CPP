e079ab2868b2b1185859c8d64837b7d7dc98aa99
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2024-11-15 20:15:53 +0530

    Add files via upload

---
+++ Basics/Patterns/Triangle Pattern/floyds_triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 4;
+    int num = 1;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = i; j > 0; j--){
+            cout<<num <<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
