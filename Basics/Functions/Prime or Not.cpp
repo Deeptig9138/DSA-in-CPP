6fd26a46bd2f48f2979b17527a946e7e6eeed96f
Deepti Gupta <156528626+Deeptig9138@users.noreply.github.com>
2025-01-01 16:17:38 +0530

    Prime or Not.cpp

---
+++ Basics/Functions/Prime or Not.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function to check if a number is prime
+bool isPrime(int number) {
+    if (number <= 1) return false;
+    for (int i = 2; i <= number / 2; i++) {
+        if (number % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
+    return 0;
+}
